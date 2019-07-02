207d897e981b8c3f11c6e245cc78f4e04fe3ff3f
Erwin Flores <ramiromarca6@gmail.com>
2019-07-01 17:02:04 -0700

    inicio

---
+++ utils/imgMtr.js
@@ -0,0 +1,94 @@
+var fs = require('fs');
+var multer=require('multer');
+
+
+function uploadImgIn(dir){
+    let diskStr={
+      destination: "./public/".concat(dir),
+      filename: function(req,res,cb){
+        cb(null, "IMG_" + Date.now() + ".jpg");
+      }
+    };
+    return multer({
+      storage: multer.diskStorage(diskStr)
+    });
+}
+
+
+
+/*
+Login USER
+*/
+
+
+//Creation of users
+router.post(/homeimg\/[a-z0-9]{1,}$/, (req, res) => {
+  var url = req.url;
+  var id = url.split("/")[2];
+  upload(req, res, (err) => {
+    if (err) {
+      res.status(500).json({
+        "msn" : "No se ha podido subir la imagen"
+      });
+    } else {
+      var ruta = req.file.path.substr(6, req.file.path.length);
+      console.log(ruta);
+      var img = {
+        idhome: id,
+        name : req.file.originalname,
+        physicalpath: req.file.path,
+        relativepath: "http://localhost:7777" + ruta
+      };
+      var imgData = new Img(img);
+      imgData.save().then( (infoimg) => {
+        //content-type
+        //Update User IMG
+        var home = {
+          gallery: new Array()
+        }
+        Home.findOne({_id:id}).exec( (err, docs) =>{
+          //console.log(docs);
+          var data = docs.gallery;
+          var aux = new  Array();
+          if (data.length == 1 && data[0] == "") {
+            home.gallery.push("/api/v1.0/homeimg/" + infoimg._id)
+          } else {
+            aux.push("/api/v1.0/homeimg/" + infoimg._id);
+            data = data.concat(aux);
+            home.gallery = data;
+          }
+          Home.findOneAndUpdate({_id : id}, home, (err, params) => {
+              if (err) {
+                res.status(500).json({
+                  "msn" : "error en la actualizacion del usuario"
+                });
+                return;
+              }
+              res.status(200).json(
+                req.file
+              );
+              return;
+          });
+        });
+      });
+    }
+  });
+});
+router.get(/homeimg\/[a-z0-9]{1,}$/, (req, res) => {
+  var url = req.url;
+  var id = url.split("/")[2];
+  console.log(id)
+  Img.findOne({_id: id}).exec((err, docs) => {
+    if (err) {
+      res.status(500).json({
+        "msn": "Sucedio algun error en el servicio"
+      });
+      return;
+    }
+    //regresamos la imagen deseada
+    var img = fs.readFileSync("./" + docs.physicalpath);
+    //var img = fs.readFileSync("./public/avatars/img.jpg");
+    res.contentType('image/jpeg');
+    res.status(200).send(img);
+  });
+});
